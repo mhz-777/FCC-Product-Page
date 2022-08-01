651d66dcc8acbd654c846d7991f4deff911ccb55
Mustafa <mhassanzadeh777@gmail.com>
2022-07-31 18:47:25 -0600

    Added javascript file containing function to hide/show mobile nav

---
+++ mobilenav.js
@@ -0,0 +1,10 @@
+
+// show/hide from w3schools.com
+function showNav() {
+    var nav = document.getElementById("nav-links");
+    if (nav.style.display === "none") {
+        nav.style.display = "block";
+    } else {
+        nav.style.display = "none";
+    }
+}
